f75dd37d731ea99c5af49abbe877c661cc31e732
elena.gronskaya <elena.gronskaya@gmail.com>
2023-03-26 19:40:38 +0200

    task1/addressed code review issues

--- task1/task1.js
+++ task1/task1.js
@@ -1,12 +1,15 @@
 const reverseString = (str) => {
-    const words = str.split(/\W+/).reverse();
-    const chars = str.split(/\w+/);
+    const words = str.split(/\W|_/).reverse();
+
+    const separatorsResult = str.matchAll(/\W|_/g);
+    const separators = Array.from(separatorsResult);
+
 
     let result = '';
     for(let i = 0; i < words.length; i++) {
-        result = result + words[i].split('').reverse().join('') + chars[i];
+        result = result + words[i].split('').reverse().join('') + (i < separators.length ?  separators[i] : '');
     }
     return result
 }
 
-console.log(reverseString('Ab,c,de$!'));
+console.log(reverseString('a,b$c_d'));
