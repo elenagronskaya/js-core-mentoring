874e62f160d273d0769893826266f33ecd390e55
elena.gronskaya <elena.gronskaya@gmail.com>
2023-04-06 09:17:21 +0300

    task2/update stringReverse function

--- task1/task1.js
+++ task1/task1.js
@@ -1,15 +1,28 @@
-const reverseString = (str) => {
-    const words = str.split(/\W+/).reverse().filter((item) => item !== '');
-
-    const separatorsResult = str.matchAll(/\W+|_/g);
-    const separators = Array.from(separatorsResult);
-
-    let result = '';
-    for(let i = 0; i < words.length; i++) {
-        result = result + words[i].split('').reverse().join('') + (i < separators.length ?  separators[i] : '');
-    }
-    return result
+const getLettersAndSymbols = function (str) {
+    return [...str].reduce((indexObj, item, index) => {
+        let code = item.charCodeAt();
+        if((code >= 'a'.charCodeAt() && code <= 'z'.charCodeAt()) || (code >= 'A'.charCodeAt() && code <= 'Z'.charCodeAt())) {
+            indexObj.words.push(index)
+        } else {
+            indexObj.symbols.push(index)
+        }
+        return indexObj;
+    }, {words: [], symbols: []})
 }
 
-console.log(reverseString('Ab,c,de$!'));
-console.log(reverseString('a,b$c'));
+const stringReverse = function (str) {
+    console.log(str)
+    let indexObj = getLettersAndSymbols(str);
+    return [...str].reduce((revOrder, item) => {
+        let code = item.charCodeAt();
+        if ((code >= 'a'.charCodeAt() && code <= 'z'.charCodeAt()) || (code >= 'A'.charCodeAt() && code <= 'Z'.charCodeAt())) {
+            revOrder[indexObj.words.pop()] = item
+        } else {
+            revOrder[indexObj.symbols.shift()] = item
+        }
+        return revOrder;
+    }, []).join('')
+}
+
+console.log(stringReverse('Ab,c,de$!'));
+console.log(stringReverse('a,b$c'));
