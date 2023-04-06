0b13527e2ecba57f0e2dd5fcff74ba144031a820
elenagronskaya <65788508+elenagronskaya@users.noreply.github.com>
2023-04-06 15:48:52 +0300

    task1 (#1)

---
+++ task1/task1.js
@@ -0,0 +1,28 @@
+const getLettersAndSymbols = function (str) {
+    return [...str].reduce((indexObj, item, index) => {
+        let code = item.charCodeAt();
+        if((code >= 'a'.charCodeAt() && code <= 'z'.charCodeAt()) || (code >= 'A'.charCodeAt() && code <= 'Z'.charCodeAt())) {
+            indexObj.words.push(index)
+        } else {
+            indexObj.symbols.push(index)
+        }
+        return indexObj;
+    }, {words: [], symbols: []})
+}
+
+const stringReverse = function (str) {
+    console.log(str)
+    let indexObj = getLettersAndSymbols(str);
+    return [...str].reduce((revOrder, item) => {
+        let code = item.charCodeAt();
+        if ((code >= 'a'.charCodeAt() && code <= 'z'.charCodeAt()) || (code >= 'A'.charCodeAt() && code <= 'Z'.charCodeAt())) {
+            revOrder[indexObj.words.pop()] = item
+        } else {
+            revOrder[indexObj.symbols.shift()] = item
+        }
+        return revOrder;
+    }, []).join('')
+}
+
+console.log(stringReverse('Ab,c,de$!'));
+console.log(stringReverse('a,b$c'));
